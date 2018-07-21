cb3bc741b2b2d83baae363c4d7791211c109c1fc
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-07-21 05:16:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
   
- snacks.find?{|i| i.include?(cheese_types)} 
+ snacks.find{|i| i.include?(cheese_types)} 
 
 
 
